d3e5ba450697baa9e812c94e494f81b29d3554fb
오준영 <junyoung.oh@gooroomee.com>
2024-10-03 19:28:08 +0900

    project init

---
+++ src/main/java/org/lowell/lectureregistration/LectureRegistrationApplication.java
@@ -0,0 +1,13 @@
+package org.lowell.lectureregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureRegistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LectureRegistrationApplication.class, args);
+    }
+
+}
