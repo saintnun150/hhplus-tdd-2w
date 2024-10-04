cbbbc9160518d0f92c71b28b16f757df8ae67bef
lowell <saintnun150@gmail.com>
2024-10-04 14:01:47 +0900

    fix: package 변경

--- src/main/java/org/lowell/lectureregistration/infrastructure/lectureAssignment/LectureAssignmentEntity.java
+++ src/main/java/org/lowell/lectureregistration/infrastructure/lectureAssignment/LectureAssignmentEntity.java
@@ -1,4 +1,4 @@
-package org.lowell.lectureregistration.infrastructure.teacherLecture;
+package org.lowell.lectureregistration.infrastructure.lectureAssignment;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
